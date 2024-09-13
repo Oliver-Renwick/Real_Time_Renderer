1fd9d92292170a20181b042703993cb7c704b4ee
Oliver-Renwick <oliverrenwick222@gmail.com>
2024-09-13 19:44:22 +0530

    Created Uniform Buffer, Descriptors, Pipeline for Triangle Test

--- Base_Renderer/Apps/Triangle.cpp
+++ Base_Renderer/Apps/Triangle.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4b02fd5694670e8a3b6ec5ff744ed923c749b3cb3e621a020846d6a9b2cdab8
-size 8629
+oid sha256:792576494d9621fdbc95196d221195dbed60425159988f7a5a6a1eb92c159237
+size 22668
